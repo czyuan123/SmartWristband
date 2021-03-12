fd16138a25b4eaaf696c883f8a3522d3ce58350c
czyuan123 <72660348+czyuan123@users.noreply.github.com>
2021-03-13 07:43:54 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello, world!').end();
+  });
+
+const admin = require('firebase-admin');
+const serviceAccount = require ('./serviceAccountKey.json');
+
+admin.initializeApp({
+    credential : admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+var http = require('https');
+var url = require('url');
+var dataa={"quote":"",
+	"author":""
+	};
+
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
+
+module.exports = app;
